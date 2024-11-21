d4b3bad1fde0b60f8bbd328689c74285ab639b13
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2024-11-20 22:09:11 -0500

    feat: :sparkles: Projects on work

---
+++ src/components/project.jsx
@@ -0,0 +1,10 @@
+export const Project = ({title, description, image}) => {
+    return(
+        <div className="h-[100%] w-[25vw] flex flex-col justify-center items-center flex-shrink-0">
+            <div className="h-[80%] w-[100%] bg-black">
+                <p>Holaaaaaaaaaa</p>
+            </div>
+            <p >{title}</p>
+        </div>
+    )
+}
--- src/views/projects.jsx
+++ src/views/projects.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { Project } from "../components/project"
+import { motion } from "framer-motion"; 
 
 export const Projects = () => {
+
+    const projects = [
+        {
+            title: "Enchanted Garden",
+            description: "MySQL Database",
+            image: "/images/project1.jpg"
+        },
+        {
+            title: "Enchanted Garden",
+            description: "MySQL Database",
+            image: "/images/project1.jpg"
+        },
+        {
+            title: "Enchanted Garden",
+            description: "MySQL Database",
+            image: "/images/project1.jpg"
+        },
+        {
+            title: "Enchanted Garden",
+            description: "MySQL Database",
+            image: "/images/project1.jpg"
+        },
+        {
+            title: "Enchanted Garden",
+            description: "MySQL Database",
+            image: "/images/project1.jpg"
+        }
+    ]
+    
     return(
-        <div>
-            <h2>Estas en los proyectos :)</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint aperiam delectus est? Commodi recusandae inventore doloribus, repellendus aliquid, harum sit adipisci omnis corporis similique ipsam officiis unde libero animi? Doloremque?</p>
-        </div>
+        <motion.div
+            className="h-[83vh] w-[100vw] px-10 m-0 overflow-hidden flex flex-col items-end"
+            initial={{ opacity: 0, scale: 1 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1.2 }}
+        >
+            <h2 className="text-7xl m-10 text-azul font-bold">Projects</h2>
+            <div className="h-[30vh] w-[100%] flex overflow-x-scroll gap-10 mt-10">
+                {projects.map((project, index) =>
+                    <Project key={index} title={project.title} description={project.description} image={project.image} />)}
+            </div>
+        </motion.div>
     )
 }
