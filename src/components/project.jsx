43ecfb9dcbadfccc29fe1ed12305f790c09ffee9
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2024-11-21 20:20:49 -0500

    feat: :sparkles: Adding an animation to the carousel in the projects section

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,10 +1,15 @@
 export const Project = ({title, description, image}) => {
     return(
-        <div className="h-[100%] w-[25vw] flex flex-col justify-center items-center flex-shrink-0">
-            <div className="h-[80%] w-[100%] bg-black">
-                <p>Holaaaaaaaaaa</p>
+        <div className="h-[35vh] flex flex-col justify-center items-end flex-shrink-0 mx-5 overflow-hidden">
+            <div className="group relative h-[80%] w-[100%] bg-black">
+                <img src={image} alt="" className="w-[100%] h-[100%] object-cover"/>
+                <div className="absolute top-0 right-[-100%] bg-azul duration-100 ease-in-out transform group-hover:right-0 w-full h-full flex flex-col justify-evenly items-center p-4">
+                    <p className="text-white font-medium font-outfit">{description}</p>
+                    {}
+                    <button className="text-azul bg-white rounded-xl cursor-pointer font-semibold border-none outline-none px-4 py-3 ">Code</button>
+                </div>
             </div>
-            <p >{title}</p>
+            <p className="font-outfit text-lg">{title}</p>
         </div>
     )
 }
--- src/views/projects.jsx
+++ src/views/projects.jsx
@@ -1,48 +1,67 @@
 import React from "react";
 import { Project } from "../components/project"
 import { motion } from "framer-motion"; 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Projects = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    };
 
     const projects = [
         {
             title: "Enchanted Garden",
-            description: "MySQL Database",
-            image: "/images/project1.jpg"
+            description: "MySQL Database for the administration of a farm, creation of 100 queries, events, functions, procedures and triggers.",
+            image: "../../public/images/examplePicture.png",
+            technologies: [
+                ""
+            ]
         },
         {
             title: "Enchanted Garden",
-            description: "MySQL Database",
-            image: "/images/project1.jpg"
+            description: "MySQL Database for the administration of a farm, creation of 100 queries, events, functions, procedures and triggers.",
+            image: "../../public/images/examplePicture.png"
         },
         {
             title: "Enchanted Garden",
-            description: "MySQL Database",
-            image: "/images/project1.jpg"
+            description: "MySQL Database for the administration of a farm, creation of 100 queries, events, functions, procedures and triggers.",
+            image: "../../public/images/examplePicture.png"
         },
         {
             title: "Enchanted Garden",
-            description: "MySQL Database",
-            image: "/images/project1.jpg"
+            description: "MySQL Database for the administration of a farm, creation of 100 queries, events, functions, procedures and triggers.",
+            image: "../../public/images/examplePicture.png"
         },
         {
             title: "Enchanted Garden",
-            description: "MySQL Database",
-            image: "/images/project1.jpg"
+            description: "MySQL Database for the administration of a farm, creation of 100 queries, events, functions, procedures and triggers.",
+            image: "../../public/images/examplePicture.png"
         }
     ]
     
     return(
         <motion.div
-            className="h-[83vh] w-[100vw] px-10 m-0 overflow-hidden flex flex-col items-end"
+            className="h-[83vh] w-[100vw] px-20 m-0 overflow-hidden flex flex-col items-end"
             initial={{ opacity: 0, scale: 1 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1.2 }}
         >
             <h2 className="text-7xl m-10 text-azul font-bold">Projects</h2>
-            <div className="h-[30vh] w-[100%] flex overflow-x-scroll gap-10 mt-10">
-                {projects.map((project, index) =>
-                    <Project key={index} title={project.title} description={project.description} image={project.image} />)}
+            <div className=" w-[100%] mt-10">
+                <Slider 
+                    {...settings}
+                >
+                    {projects.map((project, index) =>
+                        <Project key={index} title={project.title} description={project.description} image={project.image} />
+                    )}
+                </Slider>
             </div>
         </motion.div>
     )
